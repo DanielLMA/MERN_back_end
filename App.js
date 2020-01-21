880a5ad3512aa0034ffb09f16d08b0f5b253a284
Daniel Ade <Ldanielade@gmail.com>
2020-01-21 11:15:15 +1000

    heroku prep

--- App.js
+++ App.js
@@ -3,10 +3,10 @@ const app = express()
 
 const PORT = process.env.PORT || 3000
 
-app.listen(port, () => {
-    console.log(`Listening to requests on http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Listening to requests on http://localhost:${PORT}`);
   });
 
 app.get('/', (req, res) => res.send("API Running"));
 
-module.exports = app
+module.exports = app 
